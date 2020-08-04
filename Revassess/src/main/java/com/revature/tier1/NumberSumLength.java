53503c9424aa18bee03c7dec0df07641b7392940
Jack McLean <mcleanojack2@gmail.com>
2020-08-04 15:45:10 -0400

    adding tier1

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,6 +3,11 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
-		return false;
-	}
+		 public static boolean power() {
+    	  int p =12;
+    	  int l = Integer.toString(p).length();
+    	  
+    	  
+    	  return true;
+}
 }
--- Revassess/src/main/java/com/revature/tier1/User.java
+++ Revassess/src/main/java/com/revature/tier1/User.java
@@ -1,4 +1,39 @@
 package com.revature.tier1;
 
 public class User {
+     public int id;
+    public String firstName;
+    public String lastName;
+    public String username;
+    public String password;
+    public String role;
+
+    		public User(int id, String firstName, String lastName, String username, String password, String role) {
+			super();
+			this.id = id;
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.username = username;
+			this.password = password;
+			this.role = role;
+		}
+		public int getId() {
+			return id;
+		}
+		public String getFirstName() {
+			return firstName;
+		}
+		public String getLastName() {
+			return lastName;
+		}
+		public String getUsername() {
+			return username;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public String getRole() {
+			return role;
+		}
+	    
 }
